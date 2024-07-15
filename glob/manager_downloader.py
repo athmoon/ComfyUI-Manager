db68e74a33245afa20468d8340cc9bb87be809c2
athmoon <athmoon@126.com>
2024-07-15 13:31:14 +0800

    Update manager_downloader.py
        if HF_ENDPOINT:
            model_url = model_url.replace('https://huggingface.co', HF_ENDPOINT)

--- glob/manager_downloader.py
+++ glob/manager_downloader.py
@@ -15,6 +15,8 @@ if aria2 is not None:
 
 
 def download_url(model_url: str, model_dir: str, filename: str):
+    if HF_ENDPOINT:
+        model_url = model_url.replace('https://huggingface.co', HF_ENDPOINT)
     if aria2:
         return aria2_download_url(model_url, model_dir, filename)
     else:
